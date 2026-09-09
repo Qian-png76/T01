eeea7e9eb613459bb869f5f08bcc5b338bed78c3
Qian-png76 <carolzqian06@gmail.com>
2026-09-09 09:41:22 +0800

    website design
    design website and more words on every file

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // Get current page filename
 const currentPage = window.location.pathname.split('/').pop();
-// Remove .html for matching
-const pageName = currentPage.replace('.html', '');
+
+// Default to 'index' if empty (in case of root domain)
+const pageName = currentPage === '' ? 'index' : currentPage.replace('.html', '');
+
 // Find the corresponding nav link and add 'active' class
 document.querySelectorAll('nav a').forEach(link => {
+  // Check if the link's href contains the page name
   if (link.getAttribute('href').includes(pageName)) {
     link.classList.add('active');
   }
